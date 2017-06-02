2c91d76ef397c25a423405afd082f1230602571c
kh0ma <khomenko.dp@gmail.com>
2017-06-02 12:31:58 +0300

    init with spring boot initializer

---
+++ src/main/java/ua/kh0ma/LeaveRequestsApplication.java
@@ -0,0 +1,12 @@
+package ua.kh0ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveRequestsApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/kh0ma/LeaveRequestsApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.kh0ma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeaveRequestsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
